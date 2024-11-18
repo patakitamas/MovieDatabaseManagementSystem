46c25b4d063ba10337a4fb7a86a4a5122c67f44d
patakitamas <pataki.tamas.20@gmail.com>
2024-11-18 13:27:24 +0100

    Add project files.

---
+++ Entities/MovieDB.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class MovieDB
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Rating { get; set; }
+    }
+}
---
+++ Entities/MovieDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class MovieDBContext : DbContext
+    {
+        public MovieDBContext(DbContextOptions<MovieDBContext> options) : base(options) { }
+
+        public DbSet<MovieDB> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MovieDB>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Director).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Genre).HasMaxLength(50).IsRequired();
+                entity.Property(e => e.Rating).HasColumnType("decimal(10,2)");
+                });
+        }
+    }
+    
+}
